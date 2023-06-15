ba841fbab56b72dd7e72cc22b93f8f0ac74a1a39
NicolasMilliard <contact@nicolasmilliard.fr>
2023-06-15 23:19:03 +0200

    Add income Redux state

--- src/navigation/Main/Main.tsx
+++ src/navigation/Main/Main.tsx
@@ -1,11 +1,14 @@
-import { FC, useState } from "react";
+import { FC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+
 import Auth from "../../screens/Auth/Auth";
 import Home from "../../screens/Home/Home";
 
 const Main: FC = () => {
-  const [annualIncome, setAnnualIncome] = useState(0);
+  const income = useSelector((state: RootState) => state.income);
 
-  return <>{annualIncome === 0 ? <Auth /> : <Home />}</>;
+  return <>{income.value === 0 ? <Auth /> : <Home />}</>;
 };
 
 export default Main;
