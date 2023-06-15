e9d39c8153d7c1216ee130b205b10632eab51c12
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-06-15 15:53:27 +0200

    Init commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#23262e",
+    borderRadius: 16,
+    marginTop: 80,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  buttonText: {
+    marginLeft: 12,
+    color: "#dceaee",
+  },
+});
+
+export default styles;
