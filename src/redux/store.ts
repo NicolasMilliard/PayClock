171546bb5024c71fc2f0899a7ef66fb795bfe5c6
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-06-16 21:11:56 +0200

    Add startTimerSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import incomeSlice from "./incomeSlice";
+import startTimerSlice from "./startTimerSlice";
 
 const rootReducer = combineReducers({
-    income: incomeSlice
+    income: incomeSlice,
+    startTimer: startTimerSlice
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
