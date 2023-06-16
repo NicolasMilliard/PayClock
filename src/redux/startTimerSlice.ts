4078483fdbfc1567255b2d352c90678a16635afd
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-06-16 21:11:03 +0200

    Init commit

---
+++ src/redux/startTimerSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type StartTimer = {
+    value: number;
+}
+
+const initialState: StartTimer = {
+    value: 0,
+}
+
+const startTimerSlice = createSlice({name: "startTimer", initialState: initialState, reducers: {
+    setStartTimerValue: (state, action) => {
+        state.value = action.payload;
+    }
+}})
+
+export const {setStartTimerValue} = startTimerSlice.actions;
+
+export default startTimerSlice.reducer;
