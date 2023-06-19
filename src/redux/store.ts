4c8388f94c23493d2bc2e5651016dbfdc4ad475e
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-06-19 18:44:34 +0200

    Add persist store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,30 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import incomeSlice from "./incomeSlice";
 import startTimerSlice from "./startTimerSlice";
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 const rootReducer = combineReducers({
     income: incomeSlice,
     startTimer: startTimerSlice
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+// Income reducer is persisted
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ["income"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
