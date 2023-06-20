9bee2df9eb3cc7f5e58f06930a66681572de7930
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-06-20 11:40:50 +0200

    Define the type for the navigation prop

--- App.tsx
+++ App.tsx
@@ -4,12 +4,17 @@ import { Provider as ReduxProvider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./src/redux/store";
 // Navigation
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, NavigationProp } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Main from "./src/navigation/Main/Main";
 import TimerScreen from "./src/screens/Timer/TimerScreen";
 import SettingsScreen from "./src/screens/Settings/SettingsScreen";
+
+// Define the type for the navigation prop
+export type ScreenNames = ["Main", "Timer", "Settings"];
+export type RootStackParamList = Record<ScreenNames[number], undefined>;
+export type StackNavigation = NavigationProp<RootStackParamList>;
 
 export default function App() {
   const Stack = createNativeStackNavigator();
