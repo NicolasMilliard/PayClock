e9d4ba57f697cd58aff292ef259341b9351daf44
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-06-20 17:23:46 +0200

    Add styles

--- src/styles/index.js
+++ src/styles/index.js
@@ -4,13 +4,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#dceaee",
-    alignItems: "center",
   },
   centeredContainer: {
     flex: 1,
     backgroundColor: "#dceaee",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    backgroundColor: "#cfe5eb",
+    fontSize: 20,
+    color: "#23262e",
+    borderRadius: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 40,
+    paddingRight: 40,
   },
 });
 
