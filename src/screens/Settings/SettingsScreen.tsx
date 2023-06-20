ac3149b1396e39fb798553f58e9ab3e0b233f99a
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-06-20 12:23:08 +0200

    Add IconTitle component

--- src/screens/Settings/SettingsScreen.tsx
+++ src/screens/Settings/SettingsScreen.tsx
@@ -1,13 +1,28 @@
 import { FC } from "react";
-import { SafeAreaView, Text } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import styles from "../../styles/index";
 
+import IconTitle from "../../components/Titles/IconTitle/IconTitle";
+
 const SettingsScreen: FC = () => {
+  const navigation = useNavigation();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>SettingsScreen</Text>
+      <IconTitle
+        imageSource={require("../../../assets/icons/go_back/go_back.png")}
+        customFunc={handleGoBack}
+        text="Settings"
+      />
     </SafeAreaView>
   );
 };
+
+const localStyles = StyleSheet.create({});
 
 export default SettingsScreen;
